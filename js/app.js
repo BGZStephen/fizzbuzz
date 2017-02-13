8ff9b15075e91eb2480f80fa02127c18f9f0587f
BGZStephen <sjw948@gmail.com>
2017-02-13 19:54:14 +0000

    fizzbuzz challenge
    added JS app styling, bound js to button and reworked function to make
    it cleaner (innerHTML instead of document.write)

--- js/app.js
+++ js/app.js
@@ -9,15 +9,22 @@ function isBuzz(i) {
   return i % buzzVal == 0; // check if i has a remained when divided by the buzz value.
 }
 
-for (var i = 1; i <= 100; i += 1) {
-  //document.write("<p>" + i + "</p>");
-  if(isFizz(i) && isBuzz(i)) { // check if fizz and buzz check return true, if so, output FizzBuzz
-    document.write("<p class='fizzbuzz'>" + i + " - FizzBuzz</p>")
-  } else if (isFizz(i)) {
-    document.write("<p class='fizz'>" + i + " - Fizz</p>") // check if fizz check return true, if so, output Fizz
-  } else if (isBuzz(i)) {
-    document.write("<p class='buzz'>" + i + " - Buzz</p>") // check if Buzz check return true, if so, output Buzz
-  } else {
-    document.write("<p>" + i + "</p>"); // if neither Fizz or Buzz or FizzBuzz, output number
+var button = document.getElementById("go-button");
+var outputLocation = document.getElementById("app-container")
+var outputHTML = "";
+
+button.onclick = function() {
+  for (var i = 1; i <= 100; i += 1) {
+    //document.write("<p>" + i + "</p>");
+    if(isFizz(i) && isBuzz(i)) { // check if fizz and buzz check return true, if so, output FizzBuzz
+      outputHTML += "<p class='fizzbuzz'>FizzBuzz</p>";
+    } else if (isFizz(i)) {
+      outputHTML += "<p class='fizz'>Fizz</p>"; // check if fizz check return true, if so, output Fizz
+    } else if (isBuzz(i)) {
+      outputHTML += "<p class='buzz'>Buzz</p>"; // check if Buzz check return true, if so, output Buzz
+    } else {
+      outputHTML += "<p>" + i + "</p>"; // if neither Fizz or Buzz or FizzBuzz, output number
+    }
   }
+  outputLocation.innerHTML = outputHTML
 }
